bcaf9d590926f3e67a873e91bd973c81e03771ad
NguyenThanhHieu0885 <hieu94dzpro@gmail.com>
2026-06-04 21:18:29 +0700

    them morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require('morgan'); // Thêm dòng 1: Nhúng thư viện vào theo dõi log
 
 const apiRoutes = require("./routes");
 const {
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev')); // Thêm dòng 2: Bật chế độ log dành cho Developer
 
 app.get("/", (req, res) => {
   res.json({
